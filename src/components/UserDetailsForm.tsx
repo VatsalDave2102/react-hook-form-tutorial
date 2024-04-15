98407d16b64895a9430462b3f5d9d7472aad3170
VatsalDave2102 <vatsaldave2002@gmail.com>
2024-04-15 21:34:55 +0530

    add field validations

--- src/components/UserDetailsForm.tsx
+++ src/components/UserDetailsForm.tsx
@@ -23,14 +23,32 @@ const UserDetailsForm = () => {
 			<form
 				className="flex flex-col gap-y-2"
 				onSubmit={handleSubmit(onSubmitHandler)} // handleSubmit will recieve form data if form validation is successful
+				noValidate // disable HTML validation
 			>
 				<div className="form-control">
 					<label htmlFor="username">Username</label>
-					<input type="text" id="username" {...register("username")} />
+					<input
+						type="text"
+						id="username"
+						{...register("username", {
+							required: { value: true, message: "Username is required" },
+						})}
+					/>
 				</div>
 				<div className="form-control">
 					<label htmlFor="email">Email</label>
-					<input type="email" id="email" {...register("email")} />
+					<input
+						type="email"
+						id="email"
+						{...register("email", {
+							required: { value: true, message: "Email is required" },
+							pattern: {
+								value:
+									/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
+								message: "Invalid email format",
+							},
+						})}
+					/>
 				</div>
 				<div>
 					<button className="px-4 py-2 bg-indigo-700 text-white rounded-md">
